02b313e5d360809c18a15f5c98fa3c32ae2c2123
dy-xiaodong <xiaodong@indouyin.cn>
2022-11-02 12:15:42 +0800

    Update config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitepress'
+import navbar from '../config/navbar'
+import sidebar from '../config/sidebar'
+
+export default defineConfig({
+    title: 'Fastjs-next',
+    themeConfig: {
+        nav: navbar.nav,
+        sidebar: sidebar,
+        editLink: {
+            pattern: "https://github.com/fastjs-team/fastjs-docs/edit/dev/:path",
+            text: "Edit this page on GitHub",
+        }
+    }
+})
