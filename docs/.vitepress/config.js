0f666857385e441988d31f709a85ae5d7fb6c76b
DY_XiaoDong <xiaodong@indouyin.cn>
2022-12-17 21:08:13 +0800

    Update docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,6 @@ import {defineConfig} from 'vitepress'
 import navbar from '../config/navbar'
 import sidebar from '../config/sidebar'
 
-
 export default defineConfig({
   lang: 'en-US',
   title: 'Fastjs-next',
