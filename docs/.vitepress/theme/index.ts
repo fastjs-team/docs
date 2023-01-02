f0a7e0a0be12bd6794a2f02870529b5294aefa6f
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-02 15:24:52 +0800

    Update theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
 import './scroll.css'
+import {selecter} from 'fastjs-next'
+
+selecter("body").el().onload = () => {
+    setInterval(() => {
+        selecter(".VPDocAsideOutline .outline-link").each(el => {
+            el.text(
+                el.text().replace(/v[0-9.]+/g, "")
+            )
+        })
+    }, 100)
+}
 
 export default DefaultTheme
