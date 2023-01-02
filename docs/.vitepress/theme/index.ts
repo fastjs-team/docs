7d9b1c9945078a756f08510ea106df83d37394c3
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-02 17:32:13 +0800

    Update v1.2.0-1

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
 import './scroll.css'
-import {selecter} from 'fastjs-next'
 
-selecter("body").el().onload = () => {
-    setInterval(() => {
-        selecter(".VPDocAsideOutline .outline-link").each(el => {
-            el.text(
-                el.text().replace(/v[0-9.]+/g, "")
-            )
-        })
-    }, 100)
+try {
+    const b = document.body
+
+    b.onload = () => {
+        setInterval(() => {
+            b.querySelectorAll(".VPDocAsideOutline .outline-link").forEach(el => {
+                el.innerHTML = el.innerHTML.replace(/v[0-9.]+/g, "")
+            })
+        }, 100)
+    }
+} catch (e) {
+
 }
 
 export default DefaultTheme
