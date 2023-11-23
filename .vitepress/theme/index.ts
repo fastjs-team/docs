463ccb5faa35708e52e458301df623c83dc21022
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-23 21:04:13 +0800

    update(theme): update vitepress theme

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,18 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
 import './scroll.css'
 
-try {
-    const b = document.body
-
-    b.onload = () => {
-        setInterval(() => {
-            b.querySelectorAll(".VPDocAsideOutline .outline-link").forEach(el => {
-                el.innerHTML = el.innerHTML.replace(/(Fastjs[A-Za-z]*)? v[0-9.]+/g, "")
-            })
-        }, 100)
-    }
-} catch (e) {
-
-}
-
 export default DefaultTheme
