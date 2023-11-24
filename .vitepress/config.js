cda58856cb0084850408dcf3bdaed2988278c36a
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-24 22:16:08 +0800

    update(vitepress): update vitepress config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,17 +1,42 @@
 import {defineConfig} from 'vitepress'
-import navbar from '../config/navbar'
-import sidebar from '../config/sidebar'
+import navbar from './navbar.js'
+import sidebar from './sidebar.js'
+import {container} from "@mdit/plugin-container";
 
 export default defineConfig({
-  lang: 'en-US',
-  title: 'Fastjs-next',
-  themeConfig: {
-    // theme config
-    nav: navbar.nav,
-    sidebar: sidebar,
-    editLink: {
-      pattern: "https://github.com/fastjs-team/fastjs-docs/edit/main/:path",
-      text: "Edit this page on GitHub",
-    }
-  }
+    lang: 'en-US',
+    title: 'Fastjs',
+    themeConfig: {
+        nav: navbar.nav,
+        sidebar: sidebar,
+        editLink: {
+            pattern: "https://github.com/fastjs-team/docs/edit/main/:path",
+            text: "Edit this page on GitHub",
+        },
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/fastjs-team/core'},
+            {icon: 'twitter', link: 'https://twitter.com/dy_xiaodong2022'}
+        ],
+    },
+    markdown: {
+        config: (md) => {
+            md.use(container, {
+                name: 'advance',
+                openRender() {
+                    return `<Advance>`
+                },
+                closeRender() {
+                    return `</Advance>`
+                }
+            }).use(container, {
+                name: 'simple',
+                openRender() {
+                    return `<Simple>`
+                },
+                closeRender() {
+                    return `</Simple>`
+                }
+            })
+        },
+    },
 })
