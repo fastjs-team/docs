ff8f7d9d0afbf4f86626a88893b8df9b3a79c4b5
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-24 16:30:41 +0800

    update(vitepress): update sidebar and navbar

---
+++ .vitepress/navbar.js
@@ -0,0 +1,25 @@
+export default {
+  "nav": [
+    {
+      "text": "Home",
+      "link": "https://fastjs.cc/"
+    },
+    {
+      "text": "Community",
+      "items": [
+        {
+          "text": "Code of Conduct",
+            "link": "/community/code-of-conduct"
+        },
+        {
+          "text": "Contributing Guide",
+          "link": "https://github.com/fastjs-team/docs/blob/.github/contributing.md"
+        },
+        {
+          "text": "Docs Contribution",
+            "link": "https://github.com/fastjs-team/docs"
+        }
+      ]
+    },
+  ]
+}
---
+++ .vitepress/sidebar.js
@@ -0,0 +1,53 @@
+const introduction = {
+  "text": "Introduction",
+  "items": [
+    {
+      "text": "Description",
+      "link": "/"
+    },
+    {
+      "text": "Version List",
+      "link": "/guide/version-list"
+    },
+    {
+      "text": "Getting Started",
+      "link": "/guide/getting-started"
+    }
+  ]
+}
+
+const contribute = {
+  "text": "Contribute",
+  "items": [
+    {
+      "text": "Become a Sponsor",
+      "link": "/community/sponsor"
+    },
+    {
+      "text": "Docs Contribution",
+      "link": "/community/docs"
+    },
+    {
+      "text": "Code Contribution",
+      "link": "/community/code"
+    },
+  ]
+}
+
+const other = {
+  "text": "Other",
+  "items": [
+    {
+      "text": "Sponsors",
+      "link": "/other/sponsor"
+    }
+  ]
+}
+
+export default {
+  "/": [
+    introduction,
+    contribute,
+    other
+  ]
+}
